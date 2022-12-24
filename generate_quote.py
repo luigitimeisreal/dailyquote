67977e3871227bc237aa048145c0bec62d7c44ed
Luigitime! <antoniors2003@gmail.com>
2022-12-24 11:40:57 +0100

    Delete old genrate_quote()

--- generate_quote.py
+++ generate_quote.py
@@ -3,20 +3,6 @@ import datetime
 
 from quotes import quotes
 
-'''
-def generate_quote():
-    last_day = None  # Variable global
-    index_quote = -1  # Variable global
-
-    if last_day != datetime.datetime.now().day:
-        last_index = index_quote
-        while index_quote == last_index:
-            index_quote = random.randint(0, len(quotes) - 1)
-        last_day = datetime.datetime.now().day
-
-    return(quotes[index_quote])
-'''
-
 def generate_quote():
     # Read the last index
     with open('index.txt') as index_file:
