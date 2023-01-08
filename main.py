7ec69b444f0a9dc5c424c0074a5bab7b16c2f76c
Jesuswag <jesuswag88@gmail.com>
2023-01-08 19:32:05 +0100

    Added digital clock

--- main.py
+++ main.py
@@ -3,8 +3,28 @@ from kivymd.uix.label import MDLabel
 from generate_quote import generate_quote
 from kivy.lang import Builder
 
+from kivy.clock import Clock
+from kivy.properties import StringProperty
+import time
+from kivymd.uix.screen import MDScreen
+
+class Main(MDScreen):
+    #properties can get updated
+    current_time = StringProperty("")
+    def __init__(self):
+        super(Main, self).__init__()
+        #this object calls every 1 second the update function
+        self.time_obj = Clock.schedule_interval(self.update_time,1)
+    
+#dt stands for delta time, and %H, %M, %S for hour, minute and second
+    def update_time(self, dt):
+        self.current_time = time.strftime("%H"+":"+"%M"+":"+"%S")
+
 class QuoteApp(MDApp):
     def build(self):
         self.theme_cls.material_style = "M3"
-        self.theme_cls.theme_style = "Dark"
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "Brown"
+        self.theme_cls.primary_hue = "200"
+        return Main()
 QuoteApp().run()
